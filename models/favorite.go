fc342658abf4a757bd1bf91f7bf78a49540c3dc1
Vincent10z <vincentsmithray@gmail.com>
2024-08-16 19:28:21 +0300

    Renaming

--- models/favorite.go
+++ models/favorite.go
@@ -15,7 +15,7 @@ type Favorite struct {
 	ID        string    `gorm:"primaryKey;type:text" json:"id"`
 	UserID    string    `gorm:"type:text" json:"user_id"`
 	User      User      `gorm:"foreignKey:UserID" json:"user"`
-	BeerID    string    `gorm:"type:text" json:"beer_id"`
+	BeerIDs   []string  `gorm:"type:text" json:"beer_id"`
 	Beer      Beer      `gorm:"foreignKey:BeerID" json:"beer"`
 	CreatedAt time.Time `gorm:"autoCreateTime" json:"created_at"`
 }
